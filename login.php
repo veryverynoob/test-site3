357efec46458eae0a6d380e4e78f8933631e0b40
veryverynoob <87370890+veryverynoob@users.noreply.github.com>
2022-08-16 12:01:33 +0300

    Update and rename action_page.php to login.php

---
+++ login.php
@@ -0,0 +1,12 @@
+<?php
+if(issets($_POST["email"]) && issets($_POST["psw"]) && issets($_POST["psw-repeat"]) && "psw"=="psw-repeat")
+{
+  $email = htmlentities($_POST["email"]);
+  $psw = htmlentities($_POST["psw"]);
+  $psw-repeat = htmlentities($_POST["psw"]);
+}
+else
+{   
+    echo "Введенные данные некорректны";
+}
+?>
